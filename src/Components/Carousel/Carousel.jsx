25c310c2e20de75524ed2e0db4a336481e49c64c
vxshugo <vxshugo@gmail.com>
2023-03-31 17:25:28 +0600

    some fix problems

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-
 function CarouselImg() {
   return (
     <div>
